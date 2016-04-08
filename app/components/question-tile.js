b23d71b1ad9e1b7b138ebb9f2ca8ea55d681d296
Mao Thygesen <maothygesen@gmail.com>
2016-04-08 09:20:03 -0700

    create a service for adding favorite questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
   updateQuestionForm: false,
 actions:{
   update(question, params) {
       this.sendAction('update', question, params);
     },
-}
+    addToFavorite(item) {
+     this.get('favoriteQuestions').add(item);
+   }
+ }
 });
 
 // delete(question) {
