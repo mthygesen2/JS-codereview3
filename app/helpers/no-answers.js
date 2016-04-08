11790d9482b4b9267f82f2b52f60d5ba86e3e760
Mao Thygesen <maothygesen@gmail.com>
2016-04-08 08:44:24 -0700

    create a no answer icon for questions

---
+++ app/helpers/no-answers.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function noAnswers(params/*, hash*/) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') <= 0) {
+    return Ember.String.htmlSafe('<i class="fa fa-comment-o"></i>')
+  }
+}
+
+export default Ember.Helper.helper(noAnswers);
---
+++ tests/unit/helpers/no-answers-test.js
@@ -0,0 +1,10 @@
+import { noAnswers } from 'js-codereview3/helpers/no-answers';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | no answers');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = noAnswers([42]);
+  assert.ok(result);
+});
